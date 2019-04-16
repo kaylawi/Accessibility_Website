c8e8fce5cc8a7523edee14b3d7448be50bf30296
Kayla Williams <kaylawi@umich.edu>
2019-04-15 23:48:05 -0400

    Third Commit

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+# This file is used to make changes to site administration page
+
 # Register your models here.
+
+from .models import IssuesReported
+admin.site.register(IssuesReported)
