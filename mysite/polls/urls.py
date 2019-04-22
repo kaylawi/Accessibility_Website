a59b61e2e47b196671172bbcdaa855fc71ec8d59
Kayla Williams <kaylawi@umich.edu>
2019-04-22 17:05:34 -0400

    commit

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:IssuesReported_id>/',views.detail, name='detail')
+    path('<int:IssuesReported_id>/',views.detail, name='detail'),
+    path('<int:IssuesReported_id>/results/', views.results, mae='results'),
+    path('<int:IssuesReported_id>/vote/', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
 from .models import IssuesReported 
+
+#This function loads the template polls/index.html and pass it a context of dictionary mapping template variable names to Python objects
+
+def index(request):
+    latest_issues_list = IssuesReported.objects.order_by('-pub_date'])[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_issues_list': latest_issues_list}
+    return render(request, 'polls/index.html', context)
+    
+    return HttpResponse(template.render(context, request))
 
 def detail(request, IssuesReported_id)
     return HttpResponse("You're looking at issue %s." % IssuesReported_id)
