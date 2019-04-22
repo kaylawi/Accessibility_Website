b76599f2f91d311d0f401e5d0db3477709d64995
Kayla Williams <kaylawi@umich.edu>
2019-04-22 17:37:03 -0400

    commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404 
+from django.shortcuts import render
+
 from .models import IssuesReported 
 
 #This function loads the template polls/index.html and pass it a context of dictionary mapping template variable names to Python objects
@@ -14,8 +15,6 @@ def index(request):
     template = loader.get_template('polls/index.html')
     context = {'latest_issues_list': latest_issues_list}
     return render(request, 'polls/index.html', context)
-    
-    return HttpResponse(template.render(context, request))
 
 def detail(request, IssuesReported_id)
     return HttpResponse("You're looking at issue %s." % IssuesReported_id)
@@ -27,4 +26,8 @@ def results(request, IssuesReported_id):
 def vote(request, IssuesReported_id):
     return HttpReponse ("You are voting on issue %s." % IssuesReported_id)
 
+# Error Message that issue doesn't exist
+def detail(request, issues_id):
+    issue = get_object_or_404(Issue, pk=issues_id)
+    return render (request, 'polls/detail.html', {'Issue': issue })
 
