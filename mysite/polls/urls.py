df0b5194cdb91eda7b560fd7685ce11fde978550
Kayla Williams <kaylawi@umich.edu>
2019-04-23 14:26:32 -0400

    commit

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,12 +4,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:user_name_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:user_name_id>/vote/', views.vote, name='vote'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:issue_id>/vote/', views.vote, name='vote'),
 ]
