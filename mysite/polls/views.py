dc1cc51836b3b3397b986bc35bad06509c6a4c44
Kayla Williams <kaylawi@umich.edu>
2019-04-24 00:40:42 -0400

    commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -38,7 +38,7 @@ class ResultsView(generic.DetailView):
 def vote(request, question_id):
     user_name = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice = Question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
