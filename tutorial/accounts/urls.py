d51c00992aebafbd195aa85b40a7746252f3ad0f
Kayla Williams <kaylawi@umich.edu>
2019-05-20 14:35:44 -0400

    commit

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url 
 from . import views
+from django.contrib.auth import login
 
 urlpatterns = [
 
     url(r'^$', views.home),
+    url(r'^login/$', login, {'template_name': 'accounts/login.html'}) 
 ]
--- tutorial/accounts/views.py
+++ tutorial/accounts/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
+    
     return render(request, 'accounts/login.html')
