381c6b692d7234deec51fd9a7aa3143ed71c7ef7
Kayla Williams <kaylawi@umich.edu>
2019-08-18 15:49:32 -0400

    commit

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 
     url(r'^$', views.home),
-    url(r'^maps/$', views.map, name="map"),
+    url(r'^/map/$', views.map, name="map"),
     url(r'^projects/$',views.projects, name="projects"),
     url(r'^resources/$',views.resources, name="resources"),
     url(r'^administration/$',views.administration, name="administration"),
